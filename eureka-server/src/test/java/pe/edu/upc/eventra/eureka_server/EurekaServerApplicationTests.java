db18acb40c096d4615bdff94fb73a2815533a64c
Arack <arack.rs@gmail.com>
2025-05-27 21:22:32 -0500

    refactor: update properties to yml

--- eureka-server/src/test/java/pe/edu/upc/eventra/eureka_server/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/pe/edu/upc/eventra/eureka_server/EurekaServerApplicationTests.java
@@ -1,10 +1,10 @@
-package pe.edu.upc.eventra.discovery_server;
+package pe.edu.upc.eventra.eureka_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DiscoveryServerApplicationTests {
+class EurekaServerApplicationTests {
 
 	@Test
 	void contextLoads() {
